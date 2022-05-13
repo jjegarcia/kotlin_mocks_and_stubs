2788c6a9b6c930c04e7f266cffd7cee4658fdb98
Jenaro Garcia <jjegarcia@hotmail.com>
2022-05-13 19:45:52 +0100

    passing mock EmailServer.kt

--- src/main/kotlin/Library.kt
+++ src/main/kotlin/Library.kt
@@ -1,4 +1,4 @@
-class Library(val movieInfo: MovieInfo) {
+class Library(val movieInfo: MovieInfo, val emailServer: EmailServer) {
     val catalogue = hashMapOf<String, Movie>()
 
     fun findMovie(imdbId: String): Movie {
@@ -8,5 +8,6 @@ class Library(val movieInfo: MovieInfo) {
     fun donate(imdbId: String) {
         val info = movieInfo.fetch(imdbId)
         catalogue.put(imdbId, Movie(info.get("title") ?: "", (info.get("year") ?: "").toInt()))
+        emailServer.send("New Movie", "All Members", arrayOf(info.get("title") ?: "", info.get("year") ?: ""))
     }
 }
--- src/test/kotlin/LibraryTest.kt
+++ src/test/kotlin/LibraryTest.kt
@@ -1,3 +1,4 @@
+import io.mockk.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -8,11 +9,32 @@ class LibraryTest {
         val title = "The Abyss"
         val year=1989
         val movieInfo:MovieInfo = StubMovieInfo(title,year)
-        val library= Library(movieInfo)
+        val library= Library(movieInfo = movieInfo, emailServer = mockk<EmailServer>(relaxed = true))
         library.donate(imdbId)
         val movie:Movie = library.findMovie(imdbId)
         assertEquals(title,movie.title)
         assertEquals(year,movie.year)
+    }
+
+    @Test
+    fun membersEmailedAboutNewTitle(){
+        val imdbId="tt12345"
+        val title = "The Abyss"
+        val year="1989"
+        val emailServer: EmailServer = mockk()
+        every { emailServer.send(any(),any(),any()) } just Runs
+
+        val movieInfo:MovieInfo= mockk()
+        val info= hashMapOf<String,String>()
+        info.put("title",title)
+        info.put("year",year)
+        every { movieInfo.fetch(any()) } returns info
+
+        val library= Library(movieInfo = movieInfo, emailServer = emailServer)
+        library.donate(imdbId)
+        verify {
+            emailServer.send(subjet = "New Movie", recepient = "All Members", params = arrayOf(title,year))
+        }
     }
 
  }
